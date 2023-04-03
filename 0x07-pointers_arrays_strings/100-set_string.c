7bd4aeaa89ccfa1650b243774177567e6e8aa6f8
mostafaateya <mostafaateya@gmail.com>
2023-04-03 14:15:16 -0700

    8. Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - needed function
+ * @s: needed first input
+ * @to: needed second input
+ * Return: needed output
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
