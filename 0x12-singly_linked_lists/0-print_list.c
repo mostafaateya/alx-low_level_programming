17ccbd38d70adf16f333a6c894906243c7c3d7fe
mostafaateya <mostafaateya@gmail.com>
2023-04-27 15:15:20 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,7 +7,6 @@
  *
  * Return: the number of nodes printed
  */
-
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
@@ -21,5 +20,6 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		s++;
 	}
+
 	return (s);
 }
