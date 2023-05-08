0c56854a7c26eb307e4662bb72b2bd4c8db80742
mostafaateya <mostafaateya@gmail.com>
2023-05-08 15:55:34 -0700

    1. Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the text file
+ * @text_content: pointer to string added to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int x1, x2, a;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+		for (a = 0; text_content[a]; a++);
+	x1 = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	x2 = write(x1, text_content, a);
+	if (x1 == -1 || x2 == -1)
+		return (-1);
+	close(x1);
+	return (1);
+}
