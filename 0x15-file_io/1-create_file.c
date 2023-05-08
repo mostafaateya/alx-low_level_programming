87a88815dedeb7b34173454d8ffe8cd5fbe29ef0
mostafaateya <mostafaateya@gmail.com>
2023-05-08 16:16:01 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,10 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
-		for (a = 0; text_content[a]; a++);
+	{
+		for (a = 0; text_content[a];)
+			a++;
+	}
 	x1 = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	x2 = write(x1, text_content, a);
 	if (x1 == -1 || x2 == -1)
